dc219e914e1988d51bf7e9cc9c33b5945ff5caad
Fredrik Gustavsson <fredrik.x.gustavsson@saabgroup.com>
2012-06-27 16:44:54 +0200

    Two activities.

---
+++ location_test/src/com/abr/testing/DisplayMessageActivity.java
@@ -0,0 +1,21 @@
+package com.abr.testing;
+
+import android.app.Activity;
+import android.content.Intent;
+import android.os.Bundle;
+import android.widget.TextView;
+
+public class DisplayMessageActivity extends Activity {
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        Intent intent = getIntent();
+        String message = intent.getStringExtra(Location_testActivity.EXTRA_MESSAGE);
+        
+        TextView textView = new TextView(this);
+        textView.setTextSize(40);
+        textView.setText(message);
+
+        setContentView(textView);
+    }
+}
