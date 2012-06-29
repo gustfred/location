6201b3ff3698ffd0aae2d8eac7f8c96aed1350bd
Fredrik <Fredrik@zinfandel>
2012-06-29 22:21:16 +0200

    Better debug info and a functions for checking laps in place (empty so far).

--- location_test/src/com/abr/testing/Location_testActivity.java
+++ location_test/src/com/abr/testing/Location_testActivity.java
@@ -14,27 +14,40 @@ import android.widget.TextView;
 public class Location_testActivity extends Activity {
     /** Called when the activity is first created. */
 	public final static String EXTRA_MESSAGE = "com.example.myapp.MESSAGE";
+    
+    //Global variables
+    public LocationManager locationManager;
+    public LocationListener locationListener;
+    public boolean startRace = true; //Shall be set to true by a button
+    public boolean raceStarted = false; //Start race clock and set this to true
+    public boolean debugOn = true; //Shall be set to true by a button
+    public int totalLaps = 0;
+    
+    
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.main);
         
-     // Acquire a reference to the system Location Manager
-        LocationManager locationManager = (LocationManager) this.getSystemService(Context.LOCATION_SERVICE);
+        // Acquire a reference to the system Location Manager
+        locationManager = (LocationManager) this.getSystemService(Context.LOCATION_SERVICE);
 
+        
+    }
+    
+    @Override
+    public void onStart(){
+    	super.onStart();
         // Define a listener that responds to location updates
-        LocationListener locationListener = new LocationListener() {
+        locationListener = new LocationListener() {
             public void onLocationChanged(Location location) {
-              // Called when a new location is found by the network location provider.
-              //makeUseOfNewLocation(location);
-              double plats_long = location.getLongitude();
-              double plats_lat = location.getLatitude();
-              String longitude_deg = location.convert(plats_long,Location.FORMAT_MINUTES);
-              String latitude_deg = location.convert(plats_lat,Location.FORMAT_MINUTES);
-              TextView debug_info = (TextView) findViewById(R.id.debug_info);
-              debug_info.setText("Longitude: " + longitude_deg + "\nLatitude: " + latitude_deg);
-
-              //setContentView(debug_info);
+            	// Called when a new location is found by the network location provider.
+            	if(startRace){
+            		checkIfNewLap(location);
+            	}
+            	if(debugOn){
+            		printDebug(location);
+            	}
             }
 
             public void onStatusChanged(String provider, int status, Bundle extras) {}
@@ -47,21 +60,58 @@ public class Location_testActivity extends Activity {
 
         // Register the listener with the Location Manager to receive location updates
         locationManager.requestLocationUpdates(LocationManager.GPS_PROVIDER, 0, 0, locationListener);
-        
     }
     
-    	
+    @Override
+    public void onStop(){
+    	super.onStop();
+    	// Remove the listener you previously added
+    	locationManager.removeUpdates(locationListener);
+    }
     
-    /** Called when the user selects the Send button */
-    /*public void sendMessage(View view) {
-        // Do something in response to button
-    	// GIT test
-    	// GIT test2
-    	Intent intent = new Intent(this, DisplayMessageActivity.class);
-    	EditText editText = (EditText) findViewById(R.id.edit_message);
-    	String message = editText.getText().toString();
-    	intent.putExtra(EXTRA_MESSAGE, message);
-    	startActivity(intent);
-    }*/
+    public void checkIfNewLap(Location location){
+    	
+    	String lapString;
+    	
+    	TextView laps = (TextView) findViewById(R.id.laps);
+    	lapString = "Varv: " + totalLaps;
+    	laps.setText(lapString);
+    	
+    }
+    
+    public void printDebug(Location location){
+    	
+    	String debugString;
+
+    	double longitude = location.getLongitude();
+    	double latitude = location.getLatitude();
+    	String longitude_deg = location.convert(longitude,Location.FORMAT_MINUTES);
+    	String latitude_deg = location.convert(latitude,Location.FORMAT_MINUTES);
+    	
+    	debugString = "Longitude: " + longitude_deg + "\nLatitude: " + latitude_deg;
+    	
+    	if(location.hasAltitude()){
+    		double altitude = location.getAltitude();
+    		debugString = debugString + "\nAltitude: " + altitude;
+    	}
+    	if(location.hasAccuracy()){
+    		float accuracy = location.getAccuracy();
+    		debugString = debugString + "\nAccuracy (m): " + accuracy;
+    	}
+    	if(location.hasSpeed()){
+    		float speed = location.getSpeed();
+    		debugString = debugString + "\nSpeed: " + speed;
+    	}
+    	if(location.hasBearing()){
+    		float bearing = location.getBearing();
+    		debugString = debugString + "\nBearing: " + bearing;
+    	} 	
+    	
+    	TextView debugInfo = (TextView) findViewById(R.id.debugInfo);
+    	debugInfo.setText(debugString);
+    	
+    	
+    }
+    
     
 }
